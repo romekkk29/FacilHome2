2b7ee8c666052b9d49f46f4b928310f0491317d7
romekkk29 <romegomez29@gmail.com>
2022-02-02 17:34:52 -0300

    vamos carajo

--- js/producto.js
+++ js/producto.js
@@ -22,9 +22,10 @@ let dot1=document.querySelector(".dot1");
 let dot2=document.querySelector(".dot2");
 let dot3=document.querySelector(".dot3");
 dot2.style.backgroundColor="rgb(196,196,196)";
+let contador=1;
 let respuesta=false;
 let respuesta2=false;
-
+let imagenes=["img/image 44.svg","img/image 55.png","img/image 44.svg"];
 let end=0;
 let start=0;
 function validar(){
@@ -33,19 +34,45 @@ function (event){
    respuesta=true;
    console.log("move");
    start =event.changedTouches[0].clientX;
- 
 })
-   
    img.addEventListener("touchend",
 function (event){
    end= event.changedTouches[0].clientX;
    respuesta2=true;
    let diferencia= end-start;
-   if(respuesta==true && respuesta2==true && diferencia>0 ){
-      img.src="img/image 44.svg";
+   
+   if(respuesta==true && respuesta2==true && diferencia>0 && contador==1 ){
+      img.src=imagenes[contador-1];
+      izquierda();
+     
+     return contador=0;
+      
    }
-   if(respuesta==true && respuesta2==true && diferencia<0 ){
-      img.src="img/image 44.svg";
+   if(respuesta==true && respuesta2==true && diferencia>0 && contador==0 ){
+      img.src=imagenes[contador+2];
+      derecha();
+      return contador=-1;
+   }
+   if(respuesta==true && respuesta2==true && diferencia>0 && contador==-1 ){
+      img.src=imagenes[contador+2];
+      centro();
+      return contador=1;
+   }
+   if(respuesta==true && respuesta2==true && diferencia<0 && contador==1 ){
+      img.src=imagenes[contador+1];
+      derecha();
+       return  contador=-1;
+   }
+   if(respuesta==true && respuesta2==true && diferencia<0 && contador==-1 ){
+      img.src=imagenes[contador+3];
+      izquierda();
+      return contador=0;
+   }
+   if(respuesta==true && respuesta2==true && diferencia<0 && contador==0 ){
+      img.src=imagenes[contador+1];
+      centro();
+      return contador=1;
+    
    }
 })
 }
@@ -53,20 +80,29 @@ validar();
 
 
 dot1.addEventListener("click",function(){
-   dot1.style.backgroundColor="rgb(196,196,196)";
-   dot2.style.backgroundColor="rgba(196,196,196,0.5)";
-   dot3.style.backgroundColor="rgba(196,196,196,0.5)";
+   izquierda();
    img.src="img/image 44.svg";
 })
 dot2.addEventListener("click",function(){
-   dot2.style.backgroundColor="rgb(196,196,196)";
-   dot1.style.backgroundColor="rgba(196,196,196,0.5)";
-   dot3.style.backgroundColor="rgba(196,196,196,0.5)";
+   centro();
    img.src="img/image 55.png";
 })
 dot3.addEventListener("click",function(){
+   derecha();
+   img.src="img/image 44.svg";
+})
+function centro(){
+   dot2.style.backgroundColor="rgb(196,196,196)";
+   dot1.style.backgroundColor="rgba(196,196,196,0.5)";
+   dot3.style.backgroundColor="rgba(196,196,196,0.5)";
+}
+function izquierda(){
+   dot1.style.backgroundColor="rgb(196,196,196)";
+   dot2.style.backgroundColor="rgba(196,196,196,0.5)";
+   dot3.style.backgroundColor="rgba(196,196,196,0.5)";
+}
+function derecha(){
    dot3.style.backgroundColor="rgb(196,196,196)";
    dot1.style.backgroundColor="rgba(196,196,196,0.5)";
    dot2.style.backgroundColor="rgba(196,196,196,0.5)";
-   img.src="img/image 44.svg";
-})
+}
